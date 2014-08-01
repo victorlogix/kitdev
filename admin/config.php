20e087b29152eb361bf5174bb87ec7041a5002dd
Victor Ramos <victor@logixdigital.com.br>
2014-07-31 22:02:42 -0300

    Projeto iniciado

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/kit/admin/');
+define('HTTP_CATALOG', 'http://localhost/kit/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/kit/admin/');
+define('HTTPS_CATALOG', 'http://localhost/kit/');
+
+// DIR
+define('DIR_APPLICATION', 'M:\Ferramentas\Webserver\UwAmp\www\kit/admin/');
+define('DIR_SYSTEM', 'M:\Ferramentas\Webserver\UwAmp\www\kit/system/');
+define('DIR_DATABASE', 'M:\Ferramentas\Webserver\UwAmp\www\kit/system/database/');
+define('DIR_LANGUAGE', 'M:\Ferramentas\Webserver\UwAmp\www\kit/admin/language/');
+define('DIR_TEMPLATE', 'M:\Ferramentas\Webserver\UwAmp\www\kit/admin/view/template/');
+define('DIR_CONFIG', 'M:\Ferramentas\Webserver\UwAmp\www\kit/system/config/');
+define('DIR_IMAGE', 'M:\Ferramentas\Webserver\UwAmp\www\kit/image/');
+define('DIR_CACHE', 'M:\Ferramentas\Webserver\UwAmp\www\kit/system/cache/');
+define('DIR_DOWNLOAD', 'M:\Ferramentas\Webserver\UwAmp\www\kit/download/');
+define('DIR_LOGS', 'M:\Ferramentas\Webserver\UwAmp\www\kit/system/logs/');
+define('DIR_CATALOG', 'M:\Ferramentas\Webserver\UwAmp\www\kit/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'victor');
+define('DB_PASSWORD', 'passoka2');
+define('DB_DATABASE', 'kit');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/kit/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/kit/');
+
+// DIR
+define('DIR_APPLICATION', 'M:\Ferramentas\Webserver\UwAmp\www\kit/catalog/');
+define('DIR_SYSTEM', 'M:\Ferramentas\Webserver\UwAmp\www\kit/system/');
+define('DIR_DATABASE', 'M:\Ferramentas\Webserver\UwAmp\www\kit/system/database/');
+define('DIR_LANGUAGE', 'M:\Ferramentas\Webserver\UwAmp\www\kit/catalog/language/');
+define('DIR_TEMPLATE', 'M:\Ferramentas\Webserver\UwAmp\www\kit/catalog/view/theme/');
+define('DIR_CONFIG', 'M:\Ferramentas\Webserver\UwAmp\www\kit/system/config/');
+define('DIR_IMAGE', 'M:\Ferramentas\Webserver\UwAmp\www\kit/image/');
+define('DIR_CACHE', 'M:\Ferramentas\Webserver\UwAmp\www\kit/system/cache/');
+define('DIR_DOWNLOAD', 'M:\Ferramentas\Webserver\UwAmp\www\kit/download/');
+define('DIR_LOGS', 'M:\Ferramentas\Webserver\UwAmp\www\kit/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'victor');
+define('DB_PASSWORD', 'passoka2');
+define('DB_DATABASE', 'kit');
+define('DB_PREFIX', 'oc_');
+?>
